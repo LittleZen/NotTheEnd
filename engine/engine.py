8458c4b36a6eb394e88aa356f2b4aa491ef71015
Jacopo Maria Mengarelli <j.mengarelli@we-com.it>
2024-02-23 13:22:40 +0100

    Version 1.0A - fixes, and code cleanup

--- engine/engine.py
+++ engine/engine.py
@@ -1,67 +1,83 @@
+import re
+import random
 from random import sample
 from typing import Tuple
-import random
 
-def estrai_fiche(num_fiche_da_estrarre: int, num_fiche_bianche: int, num_fiche_nere: int) -> Tuple[int, int]:
-    
-    """
-    Simula l'estrazione di num_fiche_da_estrarre fiche da un sacchetto contenente
-    num_fiche_bianche fiche bianche e num_fiche_nere fiche nere.
-    
-    :param num_fiche_da_estrarre: Numero di fiche da estrarre dal sacchetto.
-    :param num_fiche_bianche: Numero di fiche bianche nel sacchetto.
-    :param num_fiche_nere: Numero di fiche nere nel sacchetto.
-    :return: Una tupla contenente il numero di fiche bianche e nere estratte (int, int).
-    """
-    sacchetto = ["B"] * num_fiche_bianche + ["N"] * num_fiche_nere
+'''
+    Decode the command raised, and extract the params
+'''
+def decode_commands_parameter(command : str) -> list:
+    command_pattern = r'!(\w+)\((\d+(?:,\d+)*)\)'
+    match = re.search(command_pattern, command)
+    if match:
+        integer_values = match.group(2).split(',')
+        integer_values = [int(value) for value in integer_values]
+        return integer_values
+        
 
-    if num_fiche_da_estrarre > len(sacchetto):
-        raise ValueError("Il numero di fiche da estrarre supera il numero totale di fiche nel sacchetto.")
+"""
+    Simula l'estrazione di num_token_da_estrarre token da un sacchetto contenente
+    num_token_bianche token bianche e num_token_nere token nere.
     
-    fiche_estratte = sample(sacchetto, num_fiche_da_estrarre)
-    fiche_bianche_estratte = fiche_estratte.count("B")
-    fiche_nere_estratte = fiche_estratte.count("N")
+    :param num_token_da_estrarre: Numero di token da estrarre dal sacchetto.
+    :param num_token_bianche: Numero di token bianche nel sacchetto.
+    :param num_token_nere: Numero di token nere nel sacchetto.
+    :return: Una tupla contenente il numero di token bianche e nere estratte (int, int).
+"""
+def estrai_token(lowered : str) -> Tuple[int, int]:
     
-    return fiche_bianche_estratte, fiche_nere_estratte
+    numbers = decode_commands_parameter(lowered) # decode the command params
+
+    num_token_da_estrarre = numbers[0] 
+    num_token_bianche =  numbers[1] 
+    num_token_nere =  numbers[2] 
+
+    print(f">TOKEN SET: extract:{num_token_da_estrarre}, wtoken:{num_token_bianche}, btoken {num_token_nere}")
+
+    sacchetto = ["B"] * num_token_bianche + ["N"] * num_token_nere
+
+    if num_token_da_estrarre > len(sacchetto):
+        raise ValueError("Il numero di token da estrarre supera il numero totale di token nel sacchetto.")
+    
+    token_estratte = sample(sacchetto, num_token_da_estrarre)
+    token_bianche_estratte = token_estratte.count("B")
+    token_nere_estratte = token_estratte.count("N")
+    
+    return token_bianche_estratte, token_nere_estratte
 
 
-def confusion(num_fiche_da_estrarre: int, num_fiche_random: int, num_fiche_nere: int) -> Tuple[int, int]:
+"""
+    Simula l'estrazione di num_token_da_estrarre token da un sacchetto contenente
+    num_token_bianche token bianche e num_token_nere token nere.
     
-    """
-    Simula l'estrazione di num_fiche_da_estrarre fiche da un sacchetto contenente
-    num_fiche_bianche fiche bianche e num_fiche_nere fiche nere.
-    
-    :param num_fiche_da_estrarre: Numero di fiche da estrarre dal sacchetto.
-    :param num_fiche_bianche: Numero di fiche bianche nel sacchetto.
-    :param num_fiche_nere: Numero di fiche nere nel sacchetto.
-    :return: Una tupla contenente il numero di fiche bianche e nere estratte (int, int).
-    """
+    :param num_token_da_estrarre: Numero di token da estrarre dal sacchetto.
+    :param num_token_bianche: Numero di token bianche nel sacchetto.
+    :param num_token_nere: Numero di token nere nel sacchetto.
+    :return: Una tupla contenente il numero di token bianche e nere estratte (int, int).
+"""
+def confusion(lowered: str) -> Tuple[int, int]:   # [TO DO ]
+
+    numbers = decode_commands_parameter(lowered) # decode the command params
+
+    num_token_da_estrarre = numbers[0]
+    num_token_random = numbers[1]
+    num_token_nere = numbers[2]
 
     # Randomizzazione delle fish, tra bianche e nere, prima dell'estrazione finale
-    tmpBianche = random.randint(0,num_fiche_random)
-    tmpBlack = num_fiche_random - tmpBianche
-    fiche_bianche_random, fiche_nere_random = estrai_fiche((tmpBianche + tmpBlack), tmpBianche , tmpBlack)
+    tmpBianche = random.randint(0,num_token_random)
+    tmpBlack = num_token_random - tmpBianche
+    token_bianche_random, token_nere_random = estrai_token((tmpBianche + tmpBlack), tmpBianche , tmpBlack)
 
-    num_fiche_nere += fiche_nere_random
+    num_token_nere += token_nere_random
 
-    sacchetto = ["B"] * fiche_bianche_random + ["N"] * num_fiche_nere
+    sacchetto = ["B"] * token_bianche_random + ["N"] * num_token_nere
 
-    if num_fiche_da_estrarre > len(sacchetto):
-        raise ValueError("Il numero di fiche da estrarre supera il numero totale di fiche nel sacchetto.")
+    if num_token_da_estrarre > len(sacchetto):
+        raise ValueError("Il numero di token da estrarre supera il numero totale di token nel sacchetto.")
     
-    fiche_estratte = sample(sacchetto, num_fiche_da_estrarre)
-    fiche_bianche_estratte = fiche_estratte.count("B") 
-    fiche_nere_estratte = fiche_estratte.count("N") 
+    token_estratte = sample(sacchetto, num_token_da_estrarre)
+    token_bianche_estratte = token_estratte.count("B") 
+    token_nere_estratte = token_estratte.count("N") 
     
-    return fiche_bianche_estratte, fiche_nere_estratte
+    return token_bianche_estratte, token_nere_estratte
 
-
-# Esempio di utilizzo
-num_fiche_da_estrarre = 3
-num_fiche_da_randomizzare = 4
-num_fiche_bianche = 5 # solo per estrazione normale
-num_fiche_nere = 2
-
-#fiche_bianche_estratte, fiche_nere_estratte = estrai_fiche(num_fiche_da_estrarre, num_fiche_bianche, num_fiche_nere)
-fiche_bianche_estratte, fiche_nere_estratte = confusion(num_fiche_da_estrarre, num_fiche_da_randomizzare, num_fiche_nere) # num_fiche_bianche = numero di fiche random in questo caso
-print(f"Fiche bianche estratte: {fiche_bianche_estratte}, Fiche nere estratte: {fiche_nere_estratte}")
--- responses.py
+++ responses.py
@@ -1,26 +1,21 @@
 import re
 from random import randint
-from engine.engine import RollNTE
+from engine.engine import *
+
 
 def get_response(user_input: str) -> str:
     lowered: str = user_input.lower()
-
     # Check if the message starts with '!' or is null
-    if not (lowered.startswith('!')) or (lowered == ''):
+    if not lowered.startswith('!') or lowered == '':
+        print("nothing for you here")
         return None
     elif 'hello' == lowered:
         return 'Hello there!'
     elif 'roll' == lowered:
         return f'You rolled: {randint(1, 10)} between 1 and 10'
-    elif 'dice' == lowered:
-        # Using regular expression to find the pattern !roll(x,y,z)
-        match = re.search(r'!roll\((\d+),(\d+),(\d+)\)', lowered)
-        if match:
-            params = match.groups()
-            numbers = [int(param) for param in params]
-            print(f"[#] numbers: {numbers}")
-            return RollNTE(numbers)
-        else:
-            return 'Invalid roll command. Use !roll(x,y,z) where x, y, and z are integers.'
+    elif 'dice' in lowered:  # Corrected condition
+        wtoken, btoken = estrai_token(lowered)
+        return f"Hai estratto:\n\nWhite Token:{wtoken}\nBlack Token: {btoken}"    
     else:
+        print(f">>{lowered}")
         return None
